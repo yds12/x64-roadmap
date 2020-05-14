71cc02e97bd66b34e18d98e7f5ec78829373c31a
yds12 <28710965+yds12@users.noreply.github.com>
2020-05-14 12:54:46 +0200

    Fix comments for first.asm.

--- tasks/first_prog/first.asm
+++ tasks/first_prog/first.asm
@@ -8,11 +8,11 @@ section .text       ; this section is where the code lives
 
 _start:             ; this is the entry point of our program. It will be
                     ; translated into a memory address.
-  mov rax, 60        ; here we move 1 into register rax
-  mov rdi, 0        ; here we move 0 into register rbx
+  mov rax, 60       ; here we move 60 into register rax
+  mov rdi, 0        ; here we move 0 into register rdi
   syscall           ; here we make a system call: the OS will take control.
                     ; the type of system call is determined by the content of
-                    ; the register rax, in this case 1, which means exit.
+                    ; the register rax, in this case 60, which means exit.
                     ; So our program will exit here. The exit code is
-                    ; determined by the content of rbx, in this case 0,
+                    ; determined by the content of rdi, in this case 0,
                     ; which means our program terminated with success.
